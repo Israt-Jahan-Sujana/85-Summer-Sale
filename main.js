9be05d834f2998d544d981c7e30da117e97343fd
israt-jahan-sujana <isratsujana25@gmail.com>
2023-08-21 23:11:51 +0600

    js updated

--- main.js
+++ main.js
@@ -1,58 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
     const productList = document.querySelectorAll(".product");
 
-    const list = document.getElementById("list");
-    const price = document.getElementById("price");
-    const discountText = document.getElementById("discount");
-    const totalPriceText = document.getElementById("total-price");
-    const couponField = document.getElementById("coupon-field");
-    const applyBtn = document.getElementById("apply-btn");
-    const makePurchaseBtn = document.getElementById("make-purchase");
-
     let totalPrice = 0;
     let serialNumber = 1;
     let discountPercentage = 0;
     let couponApplied = false;
 
+    const applyBtn = document.getElementById("apply");
+    const PurchaseBtn = document.getElementById("make-purchase");
+    const cartList = document.getElementById("cartList");
+    const price = document.getElementById("price-text");
+    const discountText = document.getElementById("discount-text");
+    const totalPriceText = document.getElementById("total-price-text");
+    const couponField = document.getElementById("coupon-field");
+
+
+
     productList.forEach(product => {
         product.addEventListener("click", function () {
             const productName = this.querySelector(".card-title").textContent;
-            const productPrice = parseFloat(this.querySelector(".price").textContent);
+            const productPrice = parseFloat(this.querySelector(".text-stone-700").textContent);
             totalPrice += productPrice;
-
             const listItem = document.createElement("li");
             listItem.textContent = serialNumber + ". " + productName;
-            list.appendChild(listItem);
-
+            cartList.appendChild(listItem);
             price.textContent = "Total price: $" + totalPrice.toFixed(2);
-
             serialNumber++;
-
-            // Enable the button if total price is above 200 and coupon is applied
-            if (totalPrice > 200) {
-                makePurchaseBtn.removeAttribute("disabled");
+            if (totalPrice >= 200) {
+                PurchaseBtn.removeAttribute("disabled");
             }
         });
     });
 
 
     applyBtn.addEventListener("click", function () {
-        const couponCode = couponField.value;
-        if (couponCode === "SELL200") {
+        if (couponField.value == "SELL200") {
             discountPercentage = 20;
             const discountAmount = (totalPrice * discountPercentage) / 100;
             const discountedTotalPrice = totalPrice - discountAmount;
 
-            discountText.textContent = "Discount: " + discountPercentage + "%";
+            discountText.textContent = "Discount: $" + discountAmount;
             totalPriceText.textContent = "Total: $" + discountedTotalPrice.toFixed(2);
             couponApplied = true;
 
-            // applyBtn.classList.add("applied");
+            
             applyBtn.disabled = true;
 
-            // Enable the button if total price is above 200 and coupon is applied
-            if (totalPrice > 200 && couponApplied) {
-                makePurchaseBtn.removeAttribute("disabled");
+            
+            if (totalPrice >=200) {
+                PurchaseBtn.removeAttribute("disabled");
                 applyBtn.setAttribute("disabled");
             }
         } else {
@@ -61,23 +57,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-    makePurchaseBtn.addEventListener("click", function () {
+    PurchaseBtn.addEventListener("click", function () {
 
         totalPrice = 0;
         serialNumber = 1;
         discountPercentage = 0;
         couponApplied = false;
 
-        list.innerHTML = "";
+        cartList.innerHTML = "";
         price.textContent = "Total price: $0.00";
-        discountText.textContent = "Discount:";
-        totalPriceText.textContent = "Total:";
+        discountText.textContent = "Discount: $0.00";
+        totalPriceText.textContent = "Total: $0.00";
         couponField.value = "";
 
         applyBtn.disabled = false;
-        makePurchaseBtn.disabled = true;
+        PurchaseBtn.disabled = true;
 
-        // alert("Purchased");
+        // alert("Purchased");\\
         my_modal_3.showModal();
     });
 });
