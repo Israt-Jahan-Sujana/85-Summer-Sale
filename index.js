c382fd2506fe99ce02ea1e6a1b6d766a3f73f963
israt-jahan-sujana <isratsujana25@gmail.com>
2023-08-21 23:27:19 +0600

    done

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const productList = document.querySelectorAll(".product");
-
     let totalPrice = 0;
     let serialNumber = 1;
     let discountPercentage = 0;
     let couponApplied = false;
-
     const applyBtn = document.getElementById("apply");
     const PurchaseBtn = document.getElementById("make-purchase");
     const cartList = document.getElementById("cartList");
@@ -13,8 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const discountText = document.getElementById("discount-text");
     const totalPriceText = document.getElementById("total-price-text");
     const couponField = document.getElementById("coupon-field");
-
-
 
     productList.forEach(product => {
         product.addEventListener("click", function () {
@@ -32,49 +28,38 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-
     applyBtn.addEventListener("click", function () {
         if (couponField.value == "SELL200") {
             discountPercentage = 20;
             const discountAmount = (totalPrice * discountPercentage) / 100;
             const discountedTotalPrice = totalPrice - discountAmount;
-
             discountText.textContent = "Discount: $" + discountAmount;
             totalPriceText.textContent = "Total: $" + discountedTotalPrice.toFixed(2);
             couponApplied = true;
-
-            
             applyBtn.disabled = true;
-
-            
             if (totalPrice >=200) {
                 PurchaseBtn.removeAttribute("disabled");
                 applyBtn.setAttribute("disabled");
+            } else {
+                console.log("wrong coupon");
             }
-        } else {
-            alert("Invalid coupon code");
-        }
+        } 
     });
 
-
     PurchaseBtn.addEventListener("click", function () {
-
         totalPrice = 0;
         serialNumber = 1;
         discountPercentage = 0;
         couponApplied = false;
-
+        applyBtn.disabled = false;
+        PurchaseBtn.disabled = true;
+        // alert("Purchased");\\
+        my_modal_3.showModal();
         cartList.innerHTML = "";
         price.textContent = "Total price: $0.00";
         discountText.textContent = "Discount: $0.00";
         totalPriceText.textContent = "Total: $0.00";
         couponField.value = "";
-
-        applyBtn.disabled = false;
-        PurchaseBtn.disabled = true;
-
-        // alert("Purchased");\\
-        my_modal_3.showModal();
     });
 });
 
